dd1540a1d0c8e07809408abc4dccc30893f3c6ff
hbc <bcxxxxxx@gmail.com>
2012-07-20 11:12:59 +0800

    finished templates

---
+++ settings.py
@@ -0,0 +1,19 @@
+#coding: utf-8
+
+# theme meta
+name = 'cb3f20'
+version = '0.10'
+website = ''
+author = {
+        'name': 'hbc',
+        'website': 'http://youknowmymind.com'
+        }
+descriptions = {}
+
+# theme settings
+analytics = None  # your google analytics number
+
+navigation = []  # main navigation
+
+copyright = '&copy; hbc 2012'  # copyright message
+slogan = 'Look at the stars, look how they shine for you.'  # site slogan
